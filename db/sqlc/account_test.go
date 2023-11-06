18583607e1ca2925d4838a1f6adda548f514f299
gokutheengineer <engineerzubizaretta@gmail.com>
2023-11-06 16:20:01 +0300

    update account table, add tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -46,3 +46,48 @@ func TestGetAccount(t *testing.T) {
 	require.Equal(t, account_created.Owner, account_retrieved.Owner)
 	require.WithinDuration(t, account_created.CreatedAt, account_retrieved.CreatedAt, 0)
 }
+
+func TestUpdateAccount(t *testing.T) {
+	account_created := createTestAccount(t)
+
+	updateAccountParams := &UpdateAccountParams{
+		ID:      account_created.ID,
+		Balance: account_created.Balance + 1000,
+	}
+
+	account_updated, err := testStore.UpdateAccount(context.Background(), *updateAccountParams)
+	require.NoError(t, err)
+	require.NotEmpty(t, account_updated)
+
+	require.Equal(t, account_created.ID, account_updated.ID)
+	require.NotEqual(t, account_created, account_updated.Balance)
+	require.Equal(t, updateAccountParams.Balance, account_updated.Balance)
+	require.Equal(t, account_created.Currency, account_updated.Currency)
+	require.Equal(t, account_created.Owner, account_updated.Owner)
+	require.WithinDuration(t, account_created.CreatedAt, account_updated.CreatedAt, 0)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account_created := createTestAccount(t)
+
+	err := testStore.DeleteAccount(context.Background(), account_created.ID)
+	require.NoError(t, err)
+
+	account_retrieved, err := testStore.GetAccount(context.Background(), account_created.ID)
+	require.Error(t, err)
+	require.Empty(t, account_retrieved)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createTestAccount(t)
+	}
+
+	accounts, err := testStore.ListAccounts(context.Background(), ListAccountsParams{Limit: 7, Offset: 3})
+	require.NoError(t, err)
+	require.Len(t, accounts, 7)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
