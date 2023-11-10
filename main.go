ab36a4f973b1b58755fb5b3832eba65bb9637ec6
gokutheengineer <engineerzubizaretta@gmail.com>
2023-11-10 10:57:45 +0300

    init main

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/gokutheengineer/bank-backend/api"
+	db "github.com/gokutheengineer/bank-backend/db/sqlc"
+	"github.com/gokutheengineer/bank-backend/util"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	config, err := util.LoadConfig("configs")
+	if err != nil {
+		log.Fatal("can't load config file: ", err)
+	}
+
+	// establish connection to pgx
+	pgxPool, err := pgxpool.New(context.Background(), config.DB_Source)
+	if err != nil {
+		log.Fatal("can't establish connection to postgres db: ", err)
+	}
+
+	store := db.NewStore(pgxPool)
+
+	server := api.NewServer(store)
+	err = server.Start("0.0.0.0:8080")
+	if err != nil {
+		log.Fatal("can't start the server")
+	}
+}
