1f0b5d88cf876264d3678607e29491d980d967db
gokutheengineer <engineerzubizaretta@gmail.com>
2023-11-10 09:42:10 +0300

    init http server

---
+++ api/account.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/gokutheengineer/bank-backend/db/sqlc"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required",oneof=USD EUR TRY`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	createAccountArgs := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, createAccountArgs)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/gokutheengineer/bank-backend/db/sqlc"
+)
+
+// Server servers HTTP reqs
+type Server struct {
+	store  db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new htpp server
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	return server
+}
+
+// Start starts the http server on the provided address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error: ": err.Error}
+}
